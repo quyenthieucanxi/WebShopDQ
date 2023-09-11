a93c6b0f82d2ec82d7cf5cf776d81d35322ba233
Huỳnh Công Quyền <quyenhuynhcong111@gmail.com>
2023-09-11 23:43:23 +0700

    second commit

---
+++ WebShopDQ.App/Data/DatabaseContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebShopDQ.App.Models;
+
+namespace WebShopDQ.App.Data
+{
+    public class DatabaseContext : IdentityDbContext<IdentityUser>
+    {
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base (options)
+        { 
+
+        }
+
+        public DbSet<User> User { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+            // Bỏ tiền tố AspNet của các bảng: mặc định
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                var tableName = entityType.GetTableName();
+                if (tableName.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }
+            builder.Entity<User>().ToTable("useracount");
+        }
+
+
+    }
+}
---
+++ WebShopDQ.App/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebShopDQ.App.Models
+{ 
+    public class User : IdentityUser
+    {
+
+    }
+}
